6a0f6f7dfbd6f59d0ff23844b78d2b8ba6f7b6da
Candra_Tran <candratran@gmail.com>
2017-03-28 23:10:42 -0700

    Renders image as brush

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,19 @@ module.exports = {
   	filename: "./lib/bundle.js"
   },
   devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
 };
